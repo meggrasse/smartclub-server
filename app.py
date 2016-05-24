a38555e9fdcc034dc8a68932f6a2fabae4c9aed2
Sam Cohen <samc1213@gmail.com>
2016-05-24 15:58:52 -0500

    upvote and downvote now working

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask
+import json
+import pickle
+
 app = Flask(__name__)
+
+app_state = {'upvotes': 0, 'downvotes': 0}
+pickle.dump( app_state, open( "app_state.p", "wb" ) )
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/upvote')
+def upvote():
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    app_state['upvotes'] += 1
+    pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    output = json.dumps(app_state)
+    return output
+
+@app.route('/downvote')
+def downvote():
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    app_state['downvotes'] += 1
+    pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    output = json.dumps(app_state)
+    return output
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80, debug=True)
---
+++ app_state.p
@@ -0,0 +1,8 @@
+(dp0
+S'upvotes'
+p1
+I0
+sS'downvotes'
+p2
+I0
+s.
