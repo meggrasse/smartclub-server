c3fdd629f6b3ea3432574e41ececa9aad2b8cdc6
Sam Cohen <samc1213@gmail.com>
2016-05-27 15:14:51 -0500

    back to pickling

--- app.py
+++ app.py
@@ -13,32 +13,32 @@ def hello_world():
 
 @app.route('/getvotecount')
 def getvotes():
-    # app_state = pickle.load( open( "app_state.p", "rb" ) )
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
     output = json.dumps(app_state)
     return output
 
 @app.route('/upvote')
 def upvote():
-    # app_state = pickle.load( open( "app_state.p", "rb" ) )
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
     app_state['upvotes'] += 1
-    # pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    pickle.dump( app_state, open( "app_state.p", "wb" ) )
     output = json.dumps(app_state)
     return output
 
 @app.route('/downvote')
 def downvote():
-    # app_state = pickle.load( open( "app_state.p", "rb" ) )
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
     app_state['downvotes'] += 1
-    # pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    pickle.dump( app_state, open( "app_state.p", "wb" ) )
     output = json.dumps(app_state)
     return output
 
 @app.route('/clearvotes')
 def clearvotes():
-    # app_state = pickle.load( open( "app_state.p", "rb" ) )
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
     app_state['downvotes'] = 0
     app_state['upvotes'] = 0
-    # pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    pickle.dump( app_state, open( "app_state.p", "wb" ) )
     output = json.dumps(app_state)
     return output
 
@@ -46,12 +46,14 @@ def clearvotes():
 def sendtunes():
     dt = request.form['music']
     dt = json.loads(dt)
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
     app_state['music'].extend(dt)
+    pickle.dump( app_state, open( "app_state.p", "wb" ) )
     return json.dumps(app_state['music'])
 
 @app.route('/wasthereascream')
 def wasthereascream():
-    # app_state = pickle.load( open( "app_state.p", "rb" ) )
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
     if 255 in app_state['music']:
         return 'Yes'
     else:
