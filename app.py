fa9fabc51ff0ef64d80e937adf361efde7e3b37e
Sam Cohen <samc1213@gmail.com>
2016-05-27 15:45:13 -0500

    added /getappstate

--- app.py
+++ app.py
@@ -4,17 +4,25 @@ import pickle
 
 app = Flask(__name__)
 
-app_state = {'upvotes': 0, 'downvotes': 0, 'music':[]}
+app_state = {'upvotes': 0, 'downvotes': 0, 'music':[], '1':0, '2':0}
 pickle.dump( app_state, open( "app_state.p", "wb" ) )
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/getappstate')
+def getappstate():
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    return json.dumps(app_state)
+    
 @app.route('/getvotecount')
 def getvotes():
     app_state = pickle.load( open( "app_state.p", "rb" ) )
-    output = json.dumps(app_state)
+    retdict = {}
+    retdict['upvotes'] = app_state['upvotes']
+    retdict['downvotes'] = app_state['downvotes']
+    output = json.dumps(retdict)
     return output
 
 @app.route('/upvote')
@@ -68,6 +76,20 @@ def gettunes():
 def resetscream():
     app_state = pickle.load( open( "app_state.p", "rb" ) )
     app_state['music'] = []
+    pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    return 'OK'
+
+@app.route('/1')
+def one():
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    app_state['1'] += 1
+    pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    return 'OK'
+
+@app.route('/2')
+def two():
+    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    app_state['2'] += 1
     pickle.dump( app_state, open( "app_state.p", "wb" ) )
     return 'OK'
 
