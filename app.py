ea282669ac0f412263dfb1fc9fcc64c7dd74691d
Sam Cohen <samc1213@gmail.com>
2016-05-27 15:04:08 -0500

    moving everything to memory not pickle file

--- app.py
+++ app.py
@@ -13,32 +13,32 @@ def hello_world():
 
 @app.route('/getvotecount')
 def getvotes():
-    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    # app_state = pickle.load( open( "app_state.p", "rb" ) )
     output = json.dumps(app_state)
     return output
 
 @app.route('/upvote')
 def upvote():
-    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    # app_state = pickle.load( open( "app_state.p", "rb" ) )
     app_state['upvotes'] += 1
-    pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    # pickle.dump( app_state, open( "app_state.p", "wb" ) )
     output = json.dumps(app_state)
     return output
 
 @app.route('/downvote')
 def downvote():
-    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    # app_state = pickle.load( open( "app_state.p", "rb" ) )
     app_state['downvotes'] += 1
-    pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    # pickle.dump( app_state, open( "app_state.p", "wb" ) )
     output = json.dumps(app_state)
     return output
 
 @app.route('/clearvotes')
 def clearvotes():
-    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    # app_state = pickle.load( open( "app_state.p", "rb" ) )
     app_state['downvotes'] = 0
     app_state['upvotes'] = 0
-    pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    # pickle.dump( app_state, open( "app_state.p", "wb" ) )
     output = json.dumps(app_state)
     return output
 
@@ -50,7 +50,7 @@ def sendtunes():
 
 @app.route('/wasthereascream')
 def wasthereascream():
-    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    # app_state = pickle.load( open( "app_state.p", "rb" ) )
     if 255 in app_state['music']:
         return 'Yes'
     else:
@@ -62,9 +62,9 @@ def gettunes():
 
 @app.route('/resetscreamtracker')
 def resetscream():
-    app_state = pickle.load( open( "app_state.p", "rb" ) )
+    # app_state = pickle.load( open( "app_state.p", "rb" ) )
     app_state['music'] = []
-    pickle.dump( app_state, open( "app_state.p", "wb" ) )
+    # pickle.dump( app_state, open( "app_state.p", "wb" ) )
     return 'OK'
 
 if __name__ == '__main__':
