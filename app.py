c0ab6cf2b09385944932ef20018573d9d61785c2
Sam Cohen <samc1213@gmail.com>
2016-06-03 11:05:54 -0500

    changed phone number

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ class poll_for_imbalance_in_people_distribution(threading.Thread):
             if len(app_state['1']) > len(app_state['2']) and self.lastone is not 1:
                 self.lastone = 1
                 print "ONE", self.id
-                message = client.messages.create(body="Hey server, there are lots of people over by the dance floor!", to="+19145845033", from_="+1 630-755-6548")
+                message = client.messages.create(body="Hey server, there are lots of people over by the dance floor!", to="+18478263494", from_="+1 630-755-6548")
             elif len(app_state['1']) < len(app_state['2']) and self.lastone is not 2:
                 self.lastone = 2
                 print "TWO", self.id
